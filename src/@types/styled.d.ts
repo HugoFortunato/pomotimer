0c99ff0402409b91bd7b351d051de88a3cde02ac
Hugo Fortunato <hugo.fortunato@pixter.com.br>
2022-08-27 16:06:10 -0300

    fix: Remove optional mode on type props

--- src/@types/styled.d.ts
+++ src/@types/styled.d.ts
@@ -5,21 +5,21 @@ export type ThemeType = {
   primary: string
   secondary: string
 
-  gray100?: string
-  gray300?: string
-  gray400?: string
-  gray500?: string
-  gray600?: string
-  gray700?: string
-  gray800?: string
-  gray900?: string
+  gray100: string
+  gray300: string
+  gray400: string
+  gray500: string
+  gray600: string
+  gray700: string
+  gray800: string
+  gray900: string
 
-  green300?: string
-  green500?: string
-  green700?: string
+  green300: string
+  green500: string
+  green700: string
 
-  red500?: string
-  red700?: string
+  red500: string
+  red700: string
 
   yellow500?: string
 }
